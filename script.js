a239f9fbda477549ce5fbb748f7b5aafeb42b18f
Demian Dunov <157014608+DemianDunov@users.noreply.github.com>
2026-04-30 14:42:15 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-function openTask(page) { window.location.href = page; } function openTask(file) { window.location.href = file; } function downloadFile(file) { const a = document.createElement("a"); a.href = file; a.download = ""; document.body.appendChild(a); a.click(); document.body.removeChild(a); }
+function openTask(page) {
+  window.location.href = page;
+}
+
+function downloadFile(file) {
+  const a = document.createElement("a");
+  a.href = file;
+  a.download = "";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
